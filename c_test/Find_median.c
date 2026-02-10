605d0ef9c1db2ee7f21e72a746c479c9859fd713
Luoka666 <3266380141@qq.com>
2026-02-10 22:05:46 +0800

    滑动窗口练习

---
+++ c_test/Find_median.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+double findMedianSortedArrays(int* nums1, int nums1Size, int* nums2, int nums2Size) {
+    int bothNums[] = {0};
+    int bothNumsSize = sizeof(bothNums) / sizeof(int);
+    int larger = MAX(nums1Size, nums2Size);
+    for (int i = 0; i < larger; i++) {
+
+    }
+}
+int main(void) {
+
+    return 0;
+}
--- c_test/Longestsubstring_Violence.c
+++ c_test/Longestsubstring_Violence.c
@@ -1,18 +1,40 @@
 #include <stdio.h>
-
-int lengthOfLongestSubstring(char* s) {
-    int left = 0, right = 0, length = 0;
-    while (s[left] != s[right]) {
-
+#include <string.h>
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+//存入出现次数法
+int lengthOfLongestSubstring_01(char* s) {
+    int left = 0;
+    int length = 0;
+    int cnt[128] = {0};//ASCII码一共0~127个
+    for (int right = 0;s[right]; right++) {
+        cnt[s[right]]++;//记录当前字母存在+1
+        while (cnt[s[right]] > 1) {//发现重复字母
+            cnt[s[left]]--;//当前left字母数减一
+            left++;//left左滑
+        }
+        //更新长度
+        if ((right - left + 1) > length) {
+            length = right - left + 1;
+        }
     }
-
-    return 0;
-
+    return length;
+}
+//位置存入法
+int lengthOfLongestSubstring_02(char* s) {
+    int left = 0;
+    int length = 0;
+    int pos[128] = {0};//存入上次出现的位置索引+1（提醒left直接跳往左重复字母的下一索引）
+    for (int right = 0;s[right]; right++) {
+        if (pos[s[right]] > left) {//防止left往前跳
+            left = pos[s[right]];//left直接跳往左重复字母的下一索引
+        }
+        pos[s[right]] = right + 1;//存入上次出现的位置索引+1
+        length = MAX(length, right - left + 1);
+    }
+    return length;
 }
 
 int main(void) {
-    char s[] = "abcabcbb";
-    lengthOfLongestSubstring(s);
-
-    return 0;
+    char s[] = "abcdefghijklmnopqrst";
+    return lengthOfLongestSubstring_02(s);
 }
